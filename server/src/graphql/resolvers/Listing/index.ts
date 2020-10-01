00ffe89a4fb32d5fa3e6b7fbfbe23fc30faf4f83
MarlineKhavele <marlinekhavele6@gmail.com>
2020-10-01 11:09:36 +0300

    Modularizing Resolvers

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -2,10 +2,10 @@ import { ObjectId } from "mongodb";
 
 import { IResolvers } from "apollo-server-express";
 
-import { Database, Listing } from "../lib/types";
+import { Database, Listing } from "../../../lib/types";
 
 
-export const resolvers: IResolvers = {
+export const  listingResolvers: IResolvers = {
     Query: {
       listings: async (_root: undefined, _args: {}, {db}:{ db: Database}): Promise<Listing[]> => {
         return  await db.listings.find({}).toArray();
