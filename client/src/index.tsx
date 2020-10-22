1d9e5213a1c86acf019c33be5fd44ddf3f57586d
MarlineKhavele <marlinekhavele6@gmail.com>
2020-10-22 22:52:01 +0300

    Building login

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { Layout } from "antd";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
+import { Home, Host, Listing, Listings,Login, NotFound, User } from "./sections";
 import * as serviceWorker from "./serviceWorker";
+
 import "./styles/index.css";
 
 const client = new ApolloClient({
@@ -15,14 +17,17 @@ const client = new ApolloClient({
 const App = () =>{
   return (
     <Router>
+      <Layout id="app">
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route  exact path="/host" component={Host}/>
         <Route exact path="/listing/:id" component={Listing}/>
         <Route exact path="/listings/:location?" component={Listings}/>
+        <Route exact path="/login" component={Login} />
         <Route  exact path="/user/:id" component={User}/>
         <Route  component={NotFound}/>
       </Switch>
+      </Layout>
     </Router>
   )
 }
