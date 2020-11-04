f3eaedde23e4a6c184ddf6f1f90a55796224a3b7
MarlineKhavele <marlinekhavele6@gmail.com>
2020-11-04 11:08:02 +0300

    Adding viewer cookie on the client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,17 @@
-import React , { useState } from "react";
+import React , { useState,useEffect,useRef } from "react";
 import { render } from "react-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import {Affix , Layout } from "antd";
+import { ApolloClient, InMemoryCache, ApolloProvider,useMutation  } from '@apollo/client';
+import {Affix , Spin,Layout } from "antd";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {AppHeader, Home, Host, Listing, Listings,Login, NotFound, User } from "./sections";
+import {LOG_IN} from "./lib/graphql/mutations";
+import {
+  LogIn as LogInData,
+  LogInVariables
+} from "./lib/graphql/mutations/LogIn/__generated__/LogIn";
 import { Viewer } from "./lib/types";
+import { AppHeaderSkeleton,ErrorBanner } from "./lib/components";
+
 
 
 import * as serviceWorker from "./serviceWorker";
@@ -27,10 +34,40 @@ const initialViewer:Viewer = {
 
 const App = () =>{
   const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  const [logIn, { error }] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+    onCompleted: data => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+      }
+    }
+  });
+
+  const logInRef = useRef(logIn);
+
+  useEffect(() => {
+    logInRef.current();
+  }, []);
+
+  if (!viewer.didRequest && !error) {
+    return (
+      <Layout className="app-skeleton">
+        <AppHeaderSkeleton />
+        <div className="app-skeleton__spin-section">
+          <Spin size="large" tip="Launching Booking" />
+        </div>
+      </Layout>
+    );
+  }
+
+  const logInErrorBannerElement = error ? (
+    <ErrorBanner description="We weren't able to verify if you were logged in. Please try again later!" />
+  ) : null;
 
   return (
     <Router>
       <Layout id="app">
+      {logInErrorBannerElement}
+
       <Affix offsetTop={0} className="app__affix-header">
         <AppHeader viewer={viewer} setViewer={setViewer}/>
       </Affix>
