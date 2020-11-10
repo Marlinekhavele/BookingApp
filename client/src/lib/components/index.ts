82d6355232d85d2da4836037bcadaf957bd059e9
MarlineKhavele <marlinekhavele6@gmail.com>
2020-11-10 10:27:49 +0300

    Added user bookings and listings

--- client/src/lib/components/index.ts
+++ client/src/lib/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./AppHeaderSkeleton";
 export * from "./ErrorBanner";
+export * from "./ListingCard";
 export * from "./PageSkeleton";
 
--- client/src/lib/utils/index.ts
+++ client/src/lib/utils/index.ts
@@ -1,4 +1,10 @@
 import { message, notification } from "antd";
+export const iconColor = "#1890ff";
+
+export const formatListingPrice = (price: number, round = true) => {
+  const formattedListingPrice = round ? Math.round(price / 100) : price / 100;
+  return `$${formattedListingPrice}`;
+};
 
 export const displaySuccessNotification = (
   message: string,
